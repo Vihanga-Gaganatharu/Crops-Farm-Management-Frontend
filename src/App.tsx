bf3369911fffa8f1c3abc9a2947128f04bd8d4b4
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2025-01-19 07:57:03 +0530

    ALL UPDATE

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
 import './App.css'
-import {RootLayout} from "./components/RootLayout.tsx";
-import {createBrowserRouter, RouterProvider} from "react-router";
-import {StaffPage} from "./pages/StaffPage.tsx";
-import {VehiclePage} from "./pages/VehiclePage.tsx";
-import {EquipmentPage} from "./pages/EquipmentPage.tsx";
-import {UserPage} from "./pages/UserPage.tsx";
-import {FieldPage} from "./pages/FieldPage.tsx";
-import {CropPage} from "./pages/CropPage.tsx";
-import {MonitoringLogPage} from "./pages/MonitoringLogPage.tsx";
-import {DashboardPage} from "./pages/DashboardPage.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import LoginPage from "./pages/LoginPage.tsx";
+import RegisterPage from "./pages/RegisterPage.tsx";
+import {ToastContainer} from "react-toastify";
+import RootLayout from "./components/RootLayout.tsx";
+import Home from "./components/wall/Home.tsx";
+import StaffWall from "./components/wall/StaffWall.tsx";
+import VehicleWall from "./components/wall/VehicleWall.tsx";
+import FieldWall from "./components/wall/FieldWall.tsx";
+import EquWall from "./components/wall/EquWall.tsx";
+import CropWall from "./components/wall/CropWall.tsx";
+import CropDataWall from "./components/wall/CropDataWall.tsx";
 
 function App() {
-    const routes = createBrowserRouter([
-        {
-            path: '',
-            element: <RootLayout/>,
-            children: [
-                {path: '', element: <DashboardPage/>},
-                {path: '/staff', element: <StaffPage/>},
-                {path: '/vehicle', element: <VehiclePage/>},
-                {path: '/equipment', element: <EquipmentPage/>},
-                {path: '/user', element: <UserPage/>},
-                {path: '/field', element: <FieldPage/>},
-                {path: '/crop', element: <CropPage/>},
-                {path: '/log', element: <MonitoringLogPage/>}
-            ]
-        },
-    ]);
 
     return (
         <>
-            <RouterProvider router={routes}/>
+            <ToastContainer />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage/>} />
+                    <Route path="/register" element={<RegisterPage />} />
+
+                    <Route path="/dashboard" element={ <RootLayout /> } >
+                        <Route path="home" element={ <Home /> } />
+                        <Route path="staff_management" element={ <StaffWall /> } />
+                        <Route path="vehicle_management" element={ <VehicleWall /> } />
+                        <Route path="field_management" element={ <FieldWall /> } />
+                        <Route path="equ_management" element={ <EquWall /> } />
+                        <Route path="crop_management" element={ <CropWall /> } />
+                        <Route path="monitor_log" element={ <CropDataWall /> } />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
