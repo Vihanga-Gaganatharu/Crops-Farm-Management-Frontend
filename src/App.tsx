ad10c379d17f02bdd756e2dd9b94c7104803363f
Vihanga Gaganatharu <vihangagaganatharu1@gmail.com>
2025-01-19 07:07:06 +0530

    images added

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {RootLayout} from "./components/RootLayout.tsx";
+import {createBrowserRouter, RouterProvider} from "react-router";
+import {StaffPage} from "./pages/StaffPage.tsx";
+import {VehiclePage} from "./pages/VehiclePage.tsx";
+import {EquipmentPage} from "./pages/EquipmentPage.tsx";
+import {UserPage} from "./pages/UserPage.tsx";
+import {FieldPage} from "./pages/FieldPage.tsx";
+import {CropPage} from "./pages/CropPage.tsx";
+import {MonitoringLogPage} from "./pages/MonitoringLogPage.tsx";
+import {DashboardPage} from "./pages/DashboardPage.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const routes = createBrowserRouter([
+        {
+            path: '',
+            element: <RootLayout/>,
+            children: [
+                {path: '', element: <DashboardPage/>},
+                {path: '/staff', element: <StaffPage/>},
+                {path: '/vehicle', element: <VehiclePage/>},
+                {path: '/equipment', element: <EquipmentPage/>},
+                {path: '/user', element: <UserPage/>},
+                {path: '/field', element: <FieldPage/>},
+                {path: '/crop', element: <CropPage/>},
+                {path: '/log', element: <MonitoringLogPage/>}
+            ]
+        },
+    ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <RouterProvider router={routes}/>
+        </>
+    )
 }
 
 export default App
